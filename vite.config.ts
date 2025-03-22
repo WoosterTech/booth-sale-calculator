b1876927469994bafab23180139cf41cdccdae2c
Karl Wooster <karl.wooster@alleima.com>
2025-03-22 00:47:21 -0700

    add base to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://vite.dev/config/
 
 export default defineConfig({
+  base: '/booth-sale-calculator/', // Add this line
   plugins: [
     react(),
     VitePWA({
